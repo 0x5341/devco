f315b9b2a6b82d0d049a5d4424b7b67728855739
0x5341 <107183580+0x5341@users.noreply.github.com>
2026-03-07 15:27:10 +0900

    change about port & path

--- config.go
+++ config.go
@@ -7,6 +7,10 @@ type config struct {
 type plugin struct {
 	Name     string
 	Features map[string]map[string]any // bool or string
-	Port     int                       // 0 means "not need open button"
-	Path     string
+	Links    []link
+}
+
+type link struct {
+	Port int // 0 means "not need open button"
+	Path string
 }
